abaa377a58e7c6a7019818e3cdcb1807e16ee31e
Jan Segre <jan@segre.in>
2013-07-09 21:50:21 -0300

    Renomeando termo inicial de v(1) para v(0).

--- vf2012/q2/viete.h
+++ vf2012/q2/viete.h
@@ -8,26 +8,26 @@
 //
 // Chamamaremos o i-ésimo termo de v(i) por enquanto.
 //
-// v(1) = sqrt(2) / 2
-// v(2) = (sqrt(2) / 2) * (sqrt(2 + sqrt(2)) / 2)
-//      = v(1) * sqrt(2 + sqrt(2)) / 2
-// v(3) = v(2) * sqrt(2 + sqrt(2 + sqrt(2))) / 2
+// v(0) = sqrt(2) / 2
+// v(1) = (sqrt(2) / 2) * (sqrt(2 + sqrt(2)) / 2)
+//      = v(0) * sqrt(2 + sqrt(2)) / 2
+// v(2) = v(1) * sqrt(2 + sqrt(2 + sqrt(2))) / 2
 //
 // Note que precisamos generalizar mais um termo, a parte
 // que está dentro da raiz, note como esse termo se comporta:
 //
-// r(1) = sqrt(2)
-// r(2) = sqrt(2 + sqrt(2)) = sqrt(2 + r(1))
-// r(3) = sqrt(2 + sqrt(2 + sqrt(2))) = sqrt(2 + r(2))
+// r(0) = sqrt(2)
+// r(1) = sqrt(2 + sqrt(2)) = sqrt(2 + r(0))
+// r(2) = sqrt(2 + sqrt(2 + sqrt(2))) = sqrt(2 + r(1))
 //
 // Assim podemos generalizar o termo r acima:
 //
-// r(1) = sqrt(2)
+// r(0) = sqrt(2)
 // r(n) = sqrt(2 + r(n - 1))
 //
 // Com esse termo podemos generalizar o termo de Viète:
 //
-// v(1) = r(1) / 2
+// v(0) = r(0) / 2
 // v(n) = v(n - 1) * r(n) / 2
 //
 // Assim podemos começar a implementar a solução:
@@ -47,5 +47,5 @@ template<unsigned n> const double viete<n>::r = std::sqrt(2 + viete<n - 1>::r);
 template<unsigned n> const double viete<n>::v = viete<n - 1>::v * viete<n>::r / 2;
 
 // termos iniciais
-template<> const double viete<1>::r = std::sqrt(2);
-template<> const double viete<1>::v = std::sqrt(2) / 2;
+template<> const double viete<0>::r = std::sqrt(2);
+template<> const double viete<0>::v = std::sqrt(2) / 2;
