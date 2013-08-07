3ebd4f0188a5cde9c4c2d421ac7bbbb997cccb6f
Jan Segre <jan@segre.in>
2013-08-07 19:53:13 -0300

    Improving the public API for the Polynomial class.

--- polycalc/src/polycalc/Polynomial.java
+++ polycalc/src/polycalc/Polynomial.java
@@ -8,18 +8,17 @@ import java.util.regex.Pattern;
 
 public class Polynomial {
     private Double[] coefs;
+    private static Pattern term_pattern = Pattern.compile("(.*)\\*?x(?:\\^|\\*\\*)?(.*)");
 
-    // One option is to pass the
-    public Polynomial(Vector<Double> coefs) {
-        this.coefs = new Double[coefs.size()];
-        for (int i = 0; i < coefs.size(); i++)
-            this.coefs[i] = coefs.get(i);
+    // the main constructor is to save the given array as the coeficients
+    // in which the index must match the power
+    public Polynomial(Double[] coefs) {
+        this.coefs = coefs;
     }
 
-    public Polynomial(int order) {
-        coefs = new Double[order + 1];
-        for (int i = 0; i < order; i++)
-            coefs[i] = new Double(0.0);
+    // the order, "1 + 2x + x^2" has order 2
+    public int getOrder() {
+        return coefs.length - 1;
     }
 
     public Double[] getCoefs() {
@@ -31,39 +30,51 @@ public class Polynomial {
         Map<Integer, Double> terms = new HashMap<>();
         int max_order = 0;
 
+        // generate a list of terms separated by a greater than one number of spaces/plus sign
         String[] raw_terms = input.split("[\\s\\+]+");
 
+        // try to parse each term
         for (String raw_term : raw_terms) {
+            // which must yield an order (expoent of x) and a value (coeficient multiplying x)
             int term_order;
             double term_value;
 
-            Pattern p = Pattern.compile("(.*)\\*?x(?:\\^|\\*\\*)?(.*)");
-            Matcher m = p.matcher(raw_term);
+            Matcher m = term_pattern.matcher(raw_term);
 
+            // check if we have a match for a term with x
             if (m.find()) {
+                // if we do so, then group(1) has the coef and group(2) the order
                 term_value = m.group(1).isEmpty() ? 1.0 : Double.parseDouble(m.group(1));
                 term_order = m.group(2).isEmpty() ? 1 : Integer.parseInt(m.group(2));
             } else {
+                // if we don't then we'll try to parse it as an independent term (order 0)
                 term_order = 0;
                 term_value = Double.parseDouble(raw_term);
             }
 
+            // check if we have a stored coeficient for that order
             Double previous_value = terms.get(term_order);
+            // if we don't switch from null to zero so we can sum it later
             previous_value = previous_value == null ? 0.0 : previous_value;
-
+            // save the sum of the previous value with the new one,
+            // that is, aggregate coeficients with same powers
             terms.put(term_order, term_value + previous_value);
 
+            // keep a track of the highest order found so far
             if (term_order > max_order)
                 max_order = term_order;
         }
 
-        Polynomial poly = new Polynomial(max_order);
+        // allocate a polynomial with the highest order needed
+        Double[] coefs = new Double[max_order + 1];
+        // and populate it with found coeficients, terms not present
+        // on our map we allocate a zero for it
         for (int i = 0; i <= max_order; i++) {
             Double term = terms.get(i);
-            poly.coefs[i] = term == null ? new Double(0.0) : term;
+            coefs[i] = term == null ? new Double(0.0) : term;
         }
 
-        return poly;
+        return new Polynomial(coefs);
     }
 
     public String toString() {
