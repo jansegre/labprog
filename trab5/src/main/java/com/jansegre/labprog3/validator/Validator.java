cb5a43a0d9ab13ce8747c74c7fd17ca07798390d
Jan Segre <jan@segre.in>
2014-03-10 12:40:16 -0300

    Show errors when invalid.

--- trab5/src/main/java/com/jansegre/labprog3/validator/Validator.java
+++ trab5/src/main/java/com/jansegre/labprog3/validator/Validator.java
@@ -2,6 +2,8 @@ package com.jansegre.labprog3.validator;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import org.jsoup.parser.Parser;
+import org.jsoup.select.Elements;
 
 import java.io.*;
 import java.net.URLEncoder;
@@ -24,17 +26,33 @@ public class Validator {
                 "group=0";
     }
 
-    static public boolean isValid(String url) throws IOException {
-        Document doc = Jsoup.connect(validatingQueryUrl(url)).get();
-        return !invalidPattern.matcher(doc.title()).matches();
+    static public Document getValidation(String url) throws IOException {
+        return Jsoup.connect(validatingQueryUrl(url)).get();
+    }
+
+    static public boolean isValid(Document validation) throws IOException {
+        return !invalidPattern.matcher(validation.title()).matches();
+    }
+
+    static String[] errors(Document validation) {
+        Elements errorElements = validation.select(".msg_err .msg");
+        String[] errorStrings = new String[errorElements.size()];
+        for (int i = 0; i < errorStrings.length; i++)
+            errorStrings[i] = Parser.unescapeEntities(errorElements.get(i).html(), false);
+        return errorStrings;
     }
 
     static public void main(String[] args) {
         String url = args.length > 0 ? args[0] : "http://google.com/";
-        System.out.println("validating " + url);
+        System.out.println("Validating " + url + " ...");
         try {
-            boolean valid = isValid(url);
+            Document doc = getValidation(url);
+            boolean valid = isValid(doc);
             System.out.println("The document is" + (valid ? " " : " NOT ") + "VALID.");
+            if (!valid) {
+                for (String error: errors(doc))
+                    System.out.println("- " + error);
+            }
         } catch (IOException e) {
             e.printStackTrace();
         }
